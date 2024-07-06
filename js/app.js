8b61c0db72a2526371cdbc36917dbc0a2abd638a
JulianVel11 <julianvelandia18@gmail.com>
2024-07-06 16:46:50 -0500

    Cambios al diseno original, cambio al responsive y la paleta de colores

--- js/app.js
+++ js/app.js
@@ -1,51 +1,26 @@
 //Tag divs text and background effect
 const showAnimal = () =>{
-    const animatedCanineTag = document.querySelector('.canines-tag');
-    const animatedFelinesTag = document.querySelector('.felines-tag');
-    
-    animatedCanineTag.addEventListener('mouseenter',()=>{
-        animatedCanineTag.classList.add("hover");
-    })
-    animatedCanineTag.addEventListener('mouseleave', () => {
-        animatedCanineTag.classList.remove('hover');
-    });
-    
-    animatedFelinesTag.addEventListener('mouseenter',()=>{
-            animatedFelinesTag.classList.add("hover");
-    })
-    animatedFelinesTag.addEventListener('mouseleave', () => {
-        animatedFelinesTag.classList.remove('hover');
-    }); 
-}
-
-//button bounce animation
-const bounceBtn = () =>{
-    const heroBtn = document.querySelector('.hero-btn');
-
-    heroBtn.addEventListener('mouseenter', ()=>{
-        gsap.to(heroBtn,{
-            y: -60,
-            duration: 0.2,
-                onComplete:()=>{
-                    gsap.to(heroBtn,{
-                        y: 0,
-                        duration: 0.2, 
-                    })
-                }
+    const animatedCards = document.querySelectorAll('.hero-card');
+        animatedCards.forEach((card) => {
+            card.addEventListener('mouseenter', ()=>{
+            card.classList.add("hover");
         })
-    })
+        card.addEventListener('mouseleave', ()=>{
+            card.classList.remove("hover");
+        })
+    });
 }
 
 
-//Text changing every 10 seconds animation for the data container
-const dataContainer = document.querySelector('.description-container');
+//Text changing every 10 seconds animation for the general info container
+const dataContainer = document.querySelector('.paragraph-container');
 const dataContainerTexts = ['Recuerda que la mejor manera de evitar enfermedades en tus mascotas es vacunarlas!', 'Un gato atigrado llamado Stubbs fue alcalde de Talkeetna, una pequeña ciudad de Alaska, ¡durante 20 años!', 'La nariz de tu canino está formada por una combinación única que es diferente en cada perro y esto mismo pasa en las huellas dactilares.', 'Los gatos tienen un total de 32 músculos… en cada oreja.'];
 let iText = 0;
 const changeText = () =>{
     dataContainer.textContent = dataContainerTexts[iText];
     iText = (iText + 1) % dataContainerTexts.length; 
 }
-setInterval(changeText, 10000)
+setInterval(changeText, 8000)
 
 
 //Card color change animation
@@ -60,20 +35,36 @@ const changeColor = () =>{
         }
     });
     
-    tl.to('.service-card',{backgroundColor: '#FFD700'});
+    tl.to('.service-card',{backgroundColor: '#4fa1e9'});
 }
 
 
-// Footer's map location with Leaflet
-let map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// Slider Animation
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive:[
+            {
+                breakpoint: 1300,
+                settings:{
+                    slidesToShow: 2
+                }
+            },{
+                breakpoint: 1050,
+                settings:{
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
 
-L.marker([51.5, -0.09]).addTo(map)
-
-bounceBtn();
+showAnimal();
 changeText();
 changeColor();
-showAnimal();
